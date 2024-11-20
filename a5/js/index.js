8233964ed704a445c7af8d29630788ec31bbe4cd
Jon Formantes <36646177+matui3@users.noreply.github.com>
2024-11-19 20:57:03 -0800

    m09 finished

---
+++ a5/js/index.js
@@ -0,0 +1,30 @@
+let slideIndex = 1;
+
+function plusSlide(n) {
+
+}
+
+function currentSlide(n) {
+
+}
+
+function showSlides(n) {
+
+}
+
+function next() {
+    if (slideIndex > 5) {
+        slideIndex = 1;
+    }
+}
+
+function back() {
+    if (slideIndex < 1) {
+        slideIndex = 5;
+    }
+}
+
+const nextButton;
+const prevButton;
+
+
---
+++ m09/js/index.js
@@ -0,0 +1,86 @@
+let dotSize = 8
+
+const dots = []
+
+
+const mybutton = document.getElementById('clean')
+mybutton.addEventListener("click", (event) => {
+
+    var element = document.getElementsByTagName("div");
+    // console.log(element)
+    for (index = element.length - 1; index >= 0; index--) {
+        if (!element[index].classList.contains('container')) {
+            element[index].parentNode.removeChild(element[index]);
+        }
+        
+    }
+
+    // Let us stop the propagation of events
+
+    event.stopPropagation();
+});
+
+const increaseButton = document.getElementById('increase')
+const decreaseButton = document.getElementById('decrease')
+const colorSelect = document.getElementById('colorPicker')
+
+
+colorSelect.addEventListener('input', chooseColor)
+
+increaseButton.addEventListener("click", () => {
+    dotSize += 1;
+    let dotsArray = Array.from(document.getElementsByClassName("dot")); // Convert HTMLCollection to array
+    dotsArray.forEach(dot => {
+        dot.style.height = dotSize + "px";
+        dot.style.width = dotSize + "px";
+    });
+    displaySizeOfDot();
+});
+
+decreaseButton.addEventListener("click", () => {
+    if (dotSize > 5) {
+        dotSize -= 1;
+    } else {
+        dotSize = 1;
+    }
+    let dotsArray = Array.from(document.getElementsByClassName("dot")); // Convert HTMLCollection to array
+    dotsArray.forEach(dot => {
+        dot.style.height = dotSize + "px";
+        dot.style.width = dotSize + "px";
+    });
+    displaySizeOfDot();
+});
+
+addEventListener("click", (event) => {
+
+    if (event.target.closest('.container') || event.target.closest('button')) {
+        return;
+    }
+    var dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - dotSize/2) + "px";
+    dot.style.top = (event.pageY - dotSize/2) + "px";
+    dot.style.height = dotSize + "px";
+    dot.style.width = dotSize + "px";
+    dot.style.backgroundColor = colorSelect.value;
+    document.body.appendChild(dot);
+    dots.push(dot)
+    displaySizeOfDot();
+});
+
+
+function displaySizeOfDot() {
+    const dot = document.querySelector('.sizeDisplay')
+    dot.textContent = `Current size: ${dotSize}px`;
+
+}
+
+function chooseColor() {
+    const colorChoice = colorSelect.value
+
+    let dotsArray = Array.from(document.getElementsByClassName("dot"));
+    dotsArray.forEach(dot => {
+        dot.style.backgroundColor = colorChoice;
+    })
+    
+}
